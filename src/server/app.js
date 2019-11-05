6701ef1e7515988dce80c52ee4820232ed9fd16f
mohannadhanafi <mohannad.hanafi96@gmail.com>
2019-11-05 22:33:03 +0200

    edit path

--- src/server/app.js
+++ src/server/app.js
@@ -8,6 +8,9 @@ const app = express()
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
   .use(express.static(join(__dirname, '..', 'client', 'build')))
+  .get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+  })
   .use('/api/v2', Router);
 
 module.exports = app;
